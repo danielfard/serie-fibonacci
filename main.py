3579b10461f1fa67ad4a2fc168ab4bd6c18e1d23
Daniel Fuentes <daniel.fuentes@masu.mx>
2024-03-06 12:36:08 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException, Depends
+import uvicorn
+from utils import get_seeds_from_time, enviar_correo, fibonacci_series
+import re
+app = FastAPI()
+
+# Función para validar el formato del correo electrónico utilizando regex
+def validate_email_format(email: str) -> str:
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if not re.match(pattern, email):
+        raise HTTPException(status_code=400, detail="Formato de correo electrónico no válido")
+    return email
+
+# Define una ruta y su función controladora
+@app.get("/")
+async def read_root(email_1: str = Depends(validate_email_format), email_2: str = None):
+    """
+    ### Endpoint para generar la serie de Fibonacci y enviarla por correo electrónico.
+
+    **Parámetros:**
+    - *email* (str): Correo electrónico al que se enviará la serie de Fibonacci.
+
+    **Retorna:**
+    - Lista de números de la serie de Fibonacci generada.
+    """
+    try:
+        if email_2 is not None:
+            validate_email_format(email_2)
+            
+        seed_x, seed_y, n, current_time = get_seeds_from_time()
+        fibonacci_numbers = fibonacci_series(seed_x, seed_y, n + 2)
+        
+        # Convertir los elementos que no son cadenas a cadenas
+        lista_convertida = [str(x) for x in fibonacci_numbers]
+        
+        # Convertir la lista a una cadena
+        cadena = ', '.join(lista_convertida)
+        enviar_correo(cadena, email_1, email_2, current_time)
+        
+        return {"Hora":current_time, "Fibonacci List":fibonacci_numbers}
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code, detail=str(e))
+    
+# Si ejecutas este archivo directamente, uvicorn lanzará el servidor
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ utils.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import datetime 
+import smtplib 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from fastapi import HTTPException
+
+def fibonacci_series(x, y, n):
+    if n <= 0:
+        return []  # Retorna una lista vacía si n es negativo o cero
+    
+    if x == 0 and y == 0:
+        return []
+    
+    if x < 0 or y < 0:
+        return []
+    
+    series = [x, y]
+    while len(series) < n:
+        next_num = series[-1] + series[-2]  # Siguiente número es la suma de los dos últimos
+        series.append(next_num)  # Añadir el siguiente número al final de la lista
+    
+    return list(reversed(series))
+
+
+def get_seeds_from_time():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")  # Obtener la hora actual
+    current_time_split  =current_time.split(":")
+    seed_x = int(current_time_split[1])//10  # Obtener el segundo dígito para la semilla x
+    seed_y = int(current_time_split[1])%10  # Obtener el tercer dígito para la semilla y
+    seconds = int(current_time_split[2])  # Obtener los segundos para la cantidad de números
+    return seed_x, seed_y, seconds, current_time  # Devolver las semillas, los segundos y la hora actual
+
+def enviar_correo(mensaje, email_1, email_2, current_time):
+    remitente = "<danielfuentesardila@gmail.com>"
+    destinatario1 = f"<{email_1}>"
+    destinatario2 = f"<{email_2}>" if email_2 is not None else ""
+    
+    # Crear un objeto MIMEMultipart
+    email = MIMEMultipart()
+    email['From'] = remitente
+    email['To'] = destinatario1 + (", " + destinatario2 if destinatario2 else "")
+    email['Subject'] = "Prueba Técnica - Daniel Alejandro Fuentes Ardila"
+    
+    # Agregar el mensaje como parte del correo electrónico
+    mensaje = f"""<b>Hora Actual:</b> {current_time}<br><b>Serie Generada:</b> {mensaje}"""
+    
+    email.attach(MIMEText(mensaje, 'html'))
+
+    try:
+        # Configurar el servidor SMTP de Gmail
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587  # Puerto para TLS
+        smtp_username = 'danielfuentesardila@gmail.com'  # Tu dirección de correo electrónico
+        smtp_password = 'dvzw rpir xkwz bjib'  # Tu contraseña de aplicación específica para Gmail
+
+        # Iniciar una conexión segura al servidor SMTP de Gmail
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+
+         # Iniciar sesión en el servidor SMTP de Gmail
+        server.login(smtp_username, smtp_password)
+
+        # Enviar el correo electrónico
+        server.sendmail(remitente, destinatario1 + (", " + destinatario2 if destinatario2 else ""), email.as_string())
+
+        # Cerrar la conexión con el servidor SMTP
+        server.quit()
+
+        print("Correo enviado exitosamente.")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error: el mensaje no pudo enviarse.")
